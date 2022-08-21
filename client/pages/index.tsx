30cf2cd2496a6ab00a0ebeaab9fb22105de036f8
jasham.qureshi <jasham.qureshi@covalensedigital.com>
2022-08-22 01:34:12 +0530

    corrected div content

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Base Page</div>;
+  return <div className={styles.container}>Base Page From ATlanta</div>;
 };
 
 export default Home;
